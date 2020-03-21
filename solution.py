e93a44ef6f0e701892cfa473d52672228f2f1dce
Andrew-Wyn <terraformer.144@gmail.com>
2020-03-21 11:05:40 +0100

    added plot

--- solution.py
+++ solution.py
@@ -16,6 +16,7 @@ import mazeClient
 import json
 from time import sleep
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
@@ -114,7 +115,7 @@ def dfs(v: dict, last_cmd: str):
 
 def print_map():
 
-    cmap = ListedColormap(['k', 'r', 'g', 'b', 'w'])
+    cmap = ListedColormap(['k', 'r', 'g', 'b', 'w']) # codified in colormap module from matplotlib
 
     # liste che possono tornare utili per la generazione dell'istogramma nel caso le tengo, senno cambio c_s, che cosi non performa
     x_s = []
@@ -125,61 +126,200 @@ def print_map():
         x_s.append(el["x"])
         y_s.append(el["y"])
         if el["val"] == 82:
-            c_s.append("red")
+            c_s.append(1) # red
         elif el["val"] == 71:
-            c_s.append("green")
+            c_s.append(2) # green
         elif el["val"] == 66:
-            c_s.append("blue")
+            c_s.append(3) # blue
         elif el["val"] == 32:
-            c_s.append("white")
+            c_s.append(4) # white
 
-    matrix_plt = np.zeros((max(x_s) + 1, max(y_s) + 1))
+    x_max = max(x_s)
+    y_max = max(y_s)
+
+    matrix_plt = np.zeros((x_max + 1, y_max + 1))
 
     for i in range(len(x_s)):
-        col = 0
-        
-        if c_s[i] == "red":
-            col = 1
-        elif c_s[i] == "green":
-            col = 2
-        elif c_s[i] == "blue":
-            col = 3
-        elif c_s[i] == "white":
-            col = 4
-
-        matrix_plt[max(x_s) - x_s[i] + 1,max(y_s) - y_s[i] + 1] = col
+        matrix_plt[x_max - x_s[i] + 1,y_max - y_s[i] + 1] = c_s[i]
 
 
     _ = plt.matshow(matrix_plt, cmap=cmap)
-    plt.xlim((0,max(y_s) - min(y_s)+2))
-    plt.ylim((max(x_s) - min(x_s)+2, 0))
+
+    plt.xlim((0,y_max - min(y_s)+2))
+    plt.ylim((x_max - min(x_s)+2, 0))
+    
     plt.show()
 
 
-def plot_statistic():
+def plot_statistic():    
+    names = list(nodes_count.keys())
+    values = list(nodes_count.values())
 
-    x_s = []
-    y_s = []
-    c_s = []
+    fig, axs = plt.subplots(1, 3, figsize=(9, 3), sharey=True)
+    axs[0].bar(names, values)
+    axs[1].scatter(names, values)
+    axs[2].plot(names, values)
+    fig.suptitle('Categorical Plotting')
+
+    plt.show()
+
+
+
+def preprocess_data_hist():
+
+    x_colors = {}
+    y_colors = {}
+
+    # molteplici riscritture
+    for el in visited:
+
+        x_colors[el["x"]] = {
+            'red':0,
+            'green':0,
+            'blue':0,
+            'white':0
+        }
+
+        y_colors[el["y"]] = {
+            'red':0,
+            'green':0,
+            'blue':0,
+            'white':0
+        }
 
     for el in visited:
-        x_s.append(el["x"])
-        y_s.append(el["y"])
+
+        x = el["x"]
+        y = el["y"]
+
         if el["val"] == 82:
-            c_s.append("red")
+            x_colors[x]['red'] += 1
+            y_colors[y]['red'] += 1 # red
         elif el["val"] == 71:
-            c_s.append("green")
+            x_colors[x]['green'] += 1
+            y_colors[y]['green'] += 1 # green
         elif el["val"] == 66:
-            c_s.append("blue")
+            x_colors[x]['blue'] += 1
+            y_colors[y]['blue'] += 1 # blue
         elif el["val"] == 32:
-            c_s.append("white")
+            x_colors[x]['white'] += 1
+            y_colors[y]['white'] += 1 # white
+
+    x_s = []
+    x_red = []
+    x_green = []
+    x_blue = []
+    x_white = []
+
+    for key in sorted(x_colors):
+        x_s.append(key)
+        x_red.append(x_colors[key]["red"])
+        x_green.append(x_colors[key]["green"])
+        x_blue.append(x_colors[key]["blue"])
+        x_white.append(x_colors[key]["white"])
+
+    y_s = []
+    y_red = []
+    y_green = []
+    y_blue = []
+    y_white = []
+
+    for key in sorted(y_colors):
+        y_s.append(key)
+        y_red.append(y_colors[key]["red"])
+        y_green.append(y_colors[key]["green"])
+        y_blue.append(y_colors[key]["blue"])
+        y_white.append(y_colors[key]["white"])
 
 
-    fig, ax = plt.subplots()
-    ax.scatter(x_s, y_s, c = c_s)
+    return x_s, x_red, x_green, x_blue, x_white, y_s, y_red, y_green, y_blue, y_white
+    
 
-    fig.savefig("stats.png")
+
+def color_histogram():
+
+    # nested function used only for this purpose
+    def autolabel(ax, rects):
+        """Attach a text label above each bar in *rects*, displaying its height."""
+        for rect in rects:
+            height = rect.get_height()
+            ax.annotate('{}'.format(height),
+                        xy=(rect.get_x() + rect.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
+
+    x_s, x_red, x_green, x_blue, x_white, y_s, y_red, y_green, y_blue, y_white = preprocess_data_hist()
+
+    fig, (ax1, ax2) = plt.subplots(1,2)
+
+    print(y_s)
+
+    width = 0.2  # the width of the bars
+
+    # ---------------------- X
+
+    x = np.arange(len(x_s))  # the label locations
+
+    rect_red = ax1.bar(x - 3*width/2, x_red, width, label='Red', color="red")
+    rect_green = ax1.bar(x - width/2, x_green, width, label='Green', color="green")
+    rect_blue = ax1.bar(x + width/2, x_blue, width, label='Blue', color="blue")
+    rect_white = ax1.bar(x + 3*width/2, x_white, width, label='White', color="grey")
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax1.set_ylabel('Frequency')
+    ax1.set_title('Frequencies for X variable')
+    ax1.set_xticks(x)
+    ax1.set_xticklabels(x_s)
+    ax1.legend()
+
+    autolabel(ax1, rect_red)
+    autolabel(ax1, rect_blue)
+    autolabel(ax1, rect_green)
+    autolabel(ax1, rect_white)
+
+    leg = ax1.get_legend()
+    leg.legendHandles[0].set_color('red')
+    leg.legendHandles[1].set_color('green')
+    leg.legendHandles[2].set_color('blue')
+    leg.legendHandles[3].set_color('grey')
+
+
+    # ---------------------------Y
+
+    y = np.arange(len(y_s))  # the label locations
+
+    rect_red1 = ax2.bar(y - 3*width/2, y_red, width, label='Red', color="red")
+    rect_green1 = ax2.bar(y - width/2, y_green, width, label='Green', color="green")
+    rect_blue1 = ax2.bar(y + width/2, y_blue, width, label='Blue', color="blue")
+    rect_white1 = ax2.bar(y + 3*width/2, y_white, width, label='White', color="grey")
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    ax2.set_ylabel('Frequency')
+    ax2.set_title('Frequencies for Y variable')
+    ax2.set_xticks(y)
+    ax2.set_xticklabels(y_s)
+    ax2.legend()
+
+    autolabel(ax2, rect_red1)
+    autolabel(ax2, rect_blue1)
+    autolabel(ax2, rect_green1)
+    autolabel(ax2, rect_white1)
+
+    leg = ax2.get_legend()
+    leg.legendHandles[0].set_color('red')
+    leg.legendHandles[1].set_color('green')
+    leg.legendHandles[2].set_color('blue')
+    leg.legendHandles[3].set_color('grey')
+
+
+    # fig.tight_layout()
+    fig.set_size_inches(18.5, 10.5)
+
     plt.show()
+
+
+
 
 
 # ---------------- Main --------------------
@@ -206,9 +346,9 @@ if __name__ == '__main__':
     # Start exploration
     dfs(curr_node, command.GET_STATE)
 
+    # quests
     print_map()
-
-    # plot_statistic()
+    color_histogram()
 
     # Print statistics
     print(nodes_count)
