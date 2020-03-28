4bb5d8e3c8b0f4ee720fa68b7b5d77ee716a8d13
CoffeeStraw <clarantonio98@gmail.com>
2020-03-28 22:11:09 +0100

    Removed directional key support (useless), improved code

--- client_controller.py
+++ client_controller.py
@@ -3,29 +3,23 @@ from mazeClient import Commands as command
 from mazeClient import send_command
 from pynput import keyboard
 
+
 def on_press(key):
     """
-    Listen for input and move if a directional key or any of 'WASD' is pressed
+    Listen for input and move if any of 'WASD' is pressed
     Exit if any other key is pressed
     """
-
-
-    _key_ = keyboard.Key
-
-    if key == _key_.up or key.char == 'w':
-        action = command.MOVE_UP
-    elif key == _key_.down or key.char == 's':
-        action = command.MOVE_DOWN
-    elif key == _key_.left or key.char == 'a':
-        action = command.MOVE_LEFT
-    elif key == _key_.right or key.char == 'd':
-        action = command.MOVE_RIGHT
-    elif key.char == 'e':
-        action = command.GET_STATE
-    else:
+    # Not a valid key pressed?
+    if not hasattr(key, 'char'):
         return False
-
+    if not key.char in keycode_map:
+        return False
+    
+    # Map keycode to action and execute action
+    action = keycode_map[key.char]
     res = send_command(action)
+
+    # Just some print to let user have some feedback
     if action == command.GET_STATE:
         print(res)
     else:
@@ -33,6 +27,15 @@ def on_press(key):
 
 
 if __name__ == "__main__":
+    # Initialize mapping variable
+    keycode_map = {
+        'w': command.MOVE_UP,
+        'a': command.MOVE_LEFT,
+        's': command.MOVE_DOWN,
+        'd': command.MOVE_RIGHT,
+        'e': command.GET_STATE
+    }
+    
     print("INSTRUCTIONS:\n\tWASD -> Move around the maze;\n\tE -> GET STATE;\n\tAny other Key: QUIT")
     # Collect events until released
     with keyboard.Listener(on_press=on_press) as listener:
